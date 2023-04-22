768eb411e3f9ee3c17ea82610a271a4580ed0ac1
bernsrepublic <bernsrepublic@gmail.com>
2023-04-22 11:27:08 -0700

    edit and update routes complete

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+// MIDDLEWARE - should be before the ROUTES
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to an Awesome App about Breads");
@@ -15,17 +23,6 @@ app.get("/", (req, res) => {
 // Breads
 const breadsController = require("./controllers/breads_controller.js");
 app.use("/breads", breadsController);
-
-// MIDDLEWARE
-app.set("views", __dirname + "/views");
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
-
-app.use(express.static("public"));
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(methodOverride("_method"));
 
 // 404 Page
 app.get("*", (req, res) => {
