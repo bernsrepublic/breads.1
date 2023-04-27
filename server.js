85208437d8581fedb759632d09ec143dccb2d112
bernsrepublic <bernsrepublic@gmail.com>
2023-04-26 18:31:10 -0700

    mongoose installation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
 const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
 // CONFIGURATION
 require("dotenv").config();
@@ -28,7 +29,13 @@ app.use("/breads", breadsController);
 app.get("*", (req, res) => {
   res.send("404");
 });
-
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 // LISTEN
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
