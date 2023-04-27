470d17e690fb526ac0a18e91ac23eea5e61766df
bernsrepublic <bernsrepublic@gmail.com>
2023-04-26 18:37:48 -0700

    npm correction

--- server.js
+++ server.js
@@ -29,13 +29,14 @@ app.use("/breads", breadsController);
 app.get("*", (req, res) => {
   res.send("404");
 });
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("connected to mongo: ", process.env.MONGO_URI);
-  }
-);
+  });
 // LISTEN
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
